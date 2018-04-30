f5b39e3d63c22da573616080ca1e2b07712e3327
Remon Lam <remon@containerstack.io>
2018-04-30 22:38:43 +0200

    update

--- debian.sh
+++ debian.sh
@@ -2,3 +2,14 @@
 
 # Update the package index;
 sudo apt update
+
+# Install dependencies;
+sudo apt install -y \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+
+# Add the Docker GPG key;
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
